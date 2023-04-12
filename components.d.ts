9adcec1bdc0052af462f9f7d2b91fa7c22ecc887
陈灿宇 <20373737@buaa.edu.cn>
2023-04-13 00:31:48 +0800

    temp for rebase

--- components.d.ts
+++ components.d.ts
@@ -10,17 +10,23 @@ export {};
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     ElAside: typeof import('element-plus/es')['ElAside'];
+    ElAvatar: typeof import('element-plus/es')['ElAvatar'];
     ElButton: typeof import('element-plus/es')['ElButton'];
+    ElCard: typeof import('element-plus/es')['ElCard'];
     ElCol: typeof import('element-plus/es')['ElCol'];
     ElContainer: typeof import('element-plus/es')['ElContainer'];
     ElDivider: typeof import('element-plus/es')['ElDivider'];
     ElHeader: typeof import('element-plus/es')['ElHeader'];
     ElIcon: typeof import('element-plus/es')['ElIcon'];
     ElImage: typeof import('element-plus/es')['ElImage'];
+    ElInput: typeof import('element-plus/es')['ElInput'];
     ElMain: typeof import('element-plus/es')['ElMain'];
     ElMenu: typeof import('element-plus/es')['ElMenu'];
     ElMenuItem: typeof import('element-plus/es')['ElMenuItem'];
     ElRow: typeof import('element-plus/es')['ElRow'];
+    ElScrollbar: typeof import('element-plus/es')['ElScrollbar'];
+    ElTabPane: typeof import('element-plus/es')['ElTabPane'];
+    ElTabs: typeof import('element-plus/es')['ElTabs'];
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default'];
     RightBoard: typeof import('./src/components/RightBoard.vue')['default'];
     RouterLink: typeof import('vue-router')['RouterLink'];
