29c288e608d5b76514e8c185da6d4d44b4590085
陈灿宇 <20373737@buaa.edu.cn>
2023-04-12 13:14:36 +0800

    [feat](right-board):右侧看板v0.1待测接口，下一步：对接

--- components.d.ts
+++ components.d.ts
@@ -12,6 +12,7 @@ declare module '@vue/runtime-core' {
     ElAside: typeof import('element-plus/es')['ElAside'];
     ElAvatar: typeof import('element-plus/es')['ElAvatar'];
     ElButton: typeof import('element-plus/es')['ElButton'];
+    ElCard: typeof import('element-plus/es')['ElCard'];
     ElCol: typeof import('element-plus/es')['ElCol'];
     ElContainer: typeof import('element-plus/es')['ElContainer'];
     ElDivider: typeof import('element-plus/es')['ElDivider'];
--- src/components/RightBoard.vue
+++ src/components/RightBoard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div style="margin: 10px; padding: 5px; border: 2px solid #535bf2">
+  <div class="right-board">
     <el-row justify="space-between">
       <el-col :span="12">
         <h3 class="right-board-title">通知版</h3>
@@ -11,12 +11,68 @@
     <el-row justify="center">
       <el-tabs style="width: 90%">
         <el-tab-pane label="未截止">
-          <el-card v-for="item of unEndedNotices" :key="indexOf(item)">
-            <span>{{ item.title }}</span>
+          <el-card
+            v-for="item of unEndedNotices"
+            :key="item"
+            class="notice-card-simple"
+            shadow="hover"
+          >
+            <el-row justify="space-between">
+              <el-col
+                :span="8"
+                style="
+                  display: flex;
+                  align-items: center;
+                  justify-content: center;
+                "
+              >
+                <el-image
+                  style="max-width: 40px; max-height: 40px; border-radius: 20px"
+                  src="/src/assets/logo.png"
+                  :fit="'fill'"
+                >
+                </el-image>
+              </el-col>
+              <el-col
+                :span="16"
+                style="
+                  display: flex;
+                  align-items: center;
+                  flex-direction: row;
+                  flex-wrap: wrap;
+                  justify-content: left;
+                "
+              >
+                <h4
+                  style="
+                    margin: 0;
+                    white-space: nowrap;
+                    text-overflow: ellipsis;
+                    overflow: hidden;
+                  "
+                >
+                  {{ item.title }}
+                </h4>
+                <span
+                  style="
+                    white-space: nowrap;
+                    text-overflow: ellipsis;
+                    overflow: hidden;
+                  "
+                >
+                  {{ item.content }}
+                </span>
+              </el-col>
+            </el-row>
           </el-card>
         </el-tab-pane>
         <el-tab-pane label="所有">
-          <el-card v-for="item of allNotices" :key="indexOf(item)">
+          <el-card
+            v-for="item of allNotices"
+            :key="item"
+            class="notice-card-simple"
+            shadow="hover"
+          >
             <span>{{ item.title }}</span>
           </el-card>
         </el-tab-pane>
@@ -28,16 +84,36 @@
 <script setup name="RightBoard">
 import { onMounted, ref } from 'vue';
 import { getNoticeList } from '/@/api/notice.js';
-let unEndedNotices = ref([]);
+let unEndedNotices = ref([
+  {
+    title: 'notice1',
+    content: 'content of notice1-1-1',
+  },
+  {
+    title: 'notice2',
+    content: 'content of notice2',
+  },
+]);
 let allNotices = ref([]);
-onMounted(() => {
+const updateNoticeList = () => {
   const list = getNoticeList;
   // 根据截止时间分成两个array
   allNotices.value = list;
+};
+onMounted(() => {
+  updateNoticeList();
 });
 </script>
 
-<style>
+<style scoped>
+.right-board {
+  margin: 10px;
+  padding: 5px;
+  border: 2px solid #535bf2;
+  border-radius: 12px;
+  box-shadow: rgba(58, 46, 68, 0.08) 0 15px 100px 0;
+}
+
 .right-board-title {
   margin: 0;
   text-align: left;
@@ -52,5 +128,13 @@ onMounted(() => {
   left: 2px;
   background-color: #535bf2;
   box-sizing: border-box;
+}
+
+.notice-card-simple {
+  margin-bottom: 10px;
+}
+
+.el-card:deep(.el-card__body) {
+  padding: 10px 0 10px;
 }
 </style>
