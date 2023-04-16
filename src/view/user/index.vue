b1e31120f0777eacf8c2d7ab2f8268d88ab7c426
zyl <example@user.com>
2023-04-16 18:11:22 +0800

    ready to register

---
+++ src/view/user/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <DivideContainer>
+    <template #main>
+      <div class="userSetting-head"></div>
+      <div class="userSetting-body"></div>
+    </template>
+    <template #right>
+      <RightBoard></RightBoard>
+    </template>
+  </DivideContainer>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'userView',
+};
+</script>
+
+<script lang="ts" setup>
+import DivideContainer from '/@/layout/components/DivideContainer.vue';
+import RightBoard from '/@/components/RightBoard.vue';
+</script>
+
+<style scoped>
+.userSetting-head {
+  height: 150px;
+  border-style: solid;
+  border-bottom: #1a1a1a;
+}
+</style>
