92f82f559a347fb3e582149f9b774a23cf0116af
肖圣鹏 <20373358@buaa.edu.cn>
2023-04-17 10:58:55 +0800

    [feat](module): 查看模块下所有通知

--- src/api/notice.js
+++ src/api/notice.js
@@ -1,15 +1,19 @@
+/* eslint-disable camelcase */
 import { get, post } from '/@/utils/request';
 
 const URL = {
   list: 'back/notice/user',
   create: 'back/notice/create',
   undueList: 'back/notice/undue',
+  queryBlock: 'four_s/notice/queryBlock/',
 };
 
 const MockURL = {
   list: 'http://127.0.0.1:4523/m1/2544583-0-default/back/notice/user/',
   create: '',
   undueList: 'http://127.0.0.1:4523/m1/2544583-0-default/back/notice/undue/',
+  queryBlock:
+    'http://127.0.0.1:4523/m1/2544583-0-default/four_s/notice/queryBlock/',
 };
 
 const getNoticeList = async () => get({ url: MockURL.list });
@@ -18,4 +22,11 @@ const getUnduNoticeList = async () => get({ url: MockURL.undueList });
 
 const createNotice = async (data) => post(data, { url: URL.create });
 
-export { getNoticeList, createNotice, getUnduNoticeList };
+const moduleNotices = async (block_id, userid, token) =>
+  get({
+    url: MockURL.queryBlock,
+    params: { block_id: block_id },
+    headers: { userid: userid, token: token },
+  });
+
+export { getNoticeList, createNotice, getUnduNoticeList, moduleNotices };
