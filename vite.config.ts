3a2e19eec345c34c47f6e1fa6216e0bf59946196
陈灿宇 <20373737@buaa.edu.cn>
2023-04-25 15:27:21 +0800

    tmp for branch

--- vite.config.ts
+++ vite.config.ts
@@ -15,6 +15,10 @@ export const MOCK_API_TARGET_URL = 'http://127.0.0.1:4523';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    'process.env': {},
+  },
+
   plugins: [
     vue(),
     vueJsx(),
