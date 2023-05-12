a4b19a47604ee04c91b23791bd62ed671450cb4b
陈灿宇 <20373737@buaa.edu.cn>
2023-05-12 14:02:14 +0800

    [fix](color)change button color

--- src/view/user/basicInfo.vue
+++ src/view/user/basicInfo.vue
@@ -34,7 +34,13 @@
         </el-form-item>
       </el-scrollbar>
       <el-form-item class="form-button">
-        <el-button type="primary" @click="submitUserInfoChange(infoFormRef)">
+        <el-button
+          class="submit-button"
+          plain
+          color="white"
+          :dark="false"
+          @click="submitUserInfoChange(infoFormRef)"
+        >
           提交修改
         </el-button>
       </el-form-item>
@@ -214,5 +220,19 @@ export default {
 .form-button :deep(.el-button) {
   background: #9007ff;
   border-color: #9007ff;
+}
+
+.submit-button {
+  width: 7vw;
+  height: 4vh;
+  border-radius: 12px;
+  background-color: rgb(130, 36, 227);
+  background-image: linear-gradient(
+    90deg,
+    rgb(130, 36, 227) 0px,
+    rgb(169, 104, 236)
+  );
+  box-shadow: rgba(130, 36, 227, 0.5) 0 1px 2px 0;
+  font-size: medium;
 }
 </style>
