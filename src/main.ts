17474c389110642d6719b738347330944b79933d
xiaolizhang77 <115877992+xiaolizhang77@users.noreply.github.com>
2023-05-22 19:17:42 +0800

    Revert "首页"
    This reverts commit a35fba932e0da4745861d05a3a2952b4e73ce7d6.

--- src/main.ts
+++ src/main.ts
@@ -6,21 +6,17 @@ import '/@/style/style.css';
 // @ts-ignore
 import VMdEditor from '@kangc/v-md-editor';
 import '@kangc/v-md-editor/lib/style/base-editor.css';
-
-import VMdPreview from '@kangc/v-md-editor/lib/preview';
-import '@kangc/v-md-editor/lib/style/preview.css';
-
 import githubTheme from '@kangc/v-md-editor/lib/theme/github.js';
 import '@kangc/v-md-editor/lib/theme/style/github.css';
+
 import App from '/@/App.vue';
 
 VMdEditor.use(githubTheme);
-VMdPreview.use(githubTheme);
+
 const app = createApp(App);
 app.use(router);
 app.use(piniaStore);
 app.use(VMdEditor);
-app.use(VMdPreview);
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 
